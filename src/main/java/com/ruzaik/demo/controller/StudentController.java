5541a7ad25bd3c022843377e4cdd61aa718d363a
Ruzaik Ahamed <ruzaik42@gmail.com>
2022-03-13 23:13:45 +0530

    Initial commit

---
+++ src/main/java/com/ruzaik/demo/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.ruzaik.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StudentController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home()
+	{
+		return "Welcome";
+	}
+}
